186d1686328d1410caf4667c4c6abd3e5ea44a30
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-01-18 17:24:29 +0600

    how it works section done

--- src/screens/HowItWorksScreen.js
+++ src/screens/HowItWorksScreen.js
@@ -4,29 +4,25 @@ import { View, Text, StyleSheet, Image, ScrollView } from 'react-native';
 const HowItWorksScreen = () => {
   return (
     <ScrollView contentContainerStyle={styles.scrollViewContent}>
-      
       <View style={styles.whiteSection}>
         <Text style={styles.sectionTitle}>The Uber for Scholarships</Text>
         <Text style={styles.sectionSubtitle}>A platform connecting the world with financially struggling students in Government Primary Schools of Bangladesh</Text>
-        <View style={styles.iconContainer}>
-          <Image source={require('../assets/icons/appstore_small.png')} style={styles.icon} />
-          <Image source={require('../assets/icons/playstore_small.png')} style={styles.icon} />
-        </View>
+        
         <View style={styles.imageContainer}>
           <View style={styles.imageCard}>
             <Image source={require('../assets/images/app_scholarship.jpg')} style={styles.cardImage} />
-            <Text style={styles.cardTitle}>Title 1</Text>
-            <Text style={styles.cardDescription}>Description for image 1</Text>
+            <Text style={styles.cardTitle}>Transfer directly</Text>
+            <Text style={styles.cardDescription}>Your scholarship is transferred directly to your student's family on their mobile bank account.</Text>
           </View>
           <View style={styles.imageCard}>
             <Image source={require('../assets/images/app_school.png')} style={styles.cardImage} />
-            <Text style={styles.cardTitle}>Title 2</Text>
-            <Text style={styles.cardDescription}>Description for image 2</Text>
+            <Text style={styles.cardTitle}>Get school reports</Text>
+            <Text style={styles.cardDescription}>View attendance data and report cards from your student's school, until completion of Class 5.</Text>
           </View>
           <View style={styles.imageCard}>
             <Image source={require('../assets/images/app_graduation.png')} style={styles.cardImage} />
-            <Text style={styles.cardTitle}>Title 3</Text>
-            <Text style={styles.cardDescription}>Description for image 3</Text>
+            <Text style={styles.cardTitle}>Ensure a literate citizen</Text>
+            <Text style={styles.cardDescription}>Your scholarship continues until completion of class 5, when your student achieves literacy.</Text>
           </View>
         </View>
       </View>
@@ -40,24 +36,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-    height: '100%',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    color: '#21252A',
-  },
   whiteSection: {
     backgroundColor: 'white',
     width: '100%',
     padding: 20,
     alignItems: 'center',
-    height: '100%',
   },
   sectionTitle: {
     fontSize: 24,
@@ -77,23 +60,17 @@ const styles = StyleSheet.create({
     width: '60%',
     marginBottom: 20,
   },
-  icon: {
-    width: "150",
-    height: "150",
-    padding:"20" ,
-  },
+ 
   imageContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
     width: '100%',
   },
   imageCard: {
     alignItems: 'center',
-    width: '30%',
+    marginBottom: 20,
   },
   cardImage: {
-    width: '100%',
-    height: 100,
+    width: '80%',
+    height: 600,
     marginBottom: 10,
   },
   cardTitle: {
@@ -106,6 +83,7 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#21252A',
     textAlign: 'center',
+    paddingHorizontal: 20,
   },
 });
 
