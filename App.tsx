149a6f25aa94ba249f92ea648d11ba31520b65c2
Md Mahadi Hasan Rifat <mahadihasanrifat86@gmail.com>
2025-01-18 13:38:53 +0600

    removed homscreens

--- App.tsx
+++ App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import HomeScreen from './src/screens/HomeScreen';
 import BottomTabBar from './src/navigation/BottomTabBar';
 
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <HomeScreen />
       <BottomTabBar />
     </SafeAreaView>
   );
